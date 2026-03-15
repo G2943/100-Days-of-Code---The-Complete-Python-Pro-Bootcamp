36928ba83a0f79f1933e72eec2f57a79bc1f5bee
G2943 <vaibhavgole88@gmail.com>
2026-03-16 02:48:37 +0530

    Ignoring .idea folder

---
+++ Day 1/Variables/variables.py
@@ -0,0 +1,6 @@
+username = input("Enter username: ")
+print("Username is : ", username)
+
+length = len(username)
+
+print("User entered string of lenth: ", length)
--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,2 +1,2 @@
 # Write your code below this line 👇
-print("Hello World this is first program in python")
+print("Hello World")
