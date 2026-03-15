022493d39160888fe8932acee486c107abe475e7
G2943 <vaibhavgole88@gmail.com>
2026-03-16 03:50:13 +0530

    On Number opterations

--- Day 1/Variables/variables.py
+++ Day 1/Variables/variables.py
@@ -1,6 +1,6 @@
 username = input("Enter username: ")
 print("Username is : ", username)
-
+# assgning varialble
 length = len(username)
 
 print("User entered string of lenth: ", length)
--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,12 @@
-len("Hello")
+# len("Hello")
+
+# print("Hello"[-5])
+
+# large number
+
+# print(234_234_234)
+
+# print(type(234_234_234))
+
+print(type(int("123")))
+
