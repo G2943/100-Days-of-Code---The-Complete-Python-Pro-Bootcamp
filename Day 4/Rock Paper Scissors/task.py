fc86eea9d605e94d7cb5659faac12bac8dadf34c
G2943 <vaibhavgole88@gmail.com>
2026-03-19 13:07:16 +0530

    rock paper scissor to be done yet

--- Day 4/Rock Paper Scissors/task.py
+++ Day 4/Rock Paper Scissors/task.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +26,17 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+
+user_choice = input("Enter your choice: 1 for Rock, 2 for Paper, 3 for Scissors \n")
+
+comp_choice = random.randint(1, 3)
+
+if(user_choice == 1 && comp_choice == 1):
+    print("Match Tie!")
+elif(user_choice == 1 && comp_choice == 2):
+    print("Computer Wins!")
+elif(user_choice == 1 && comp_choice == 3):
+    print("You win!")
+elif(user_choice == 2 && comp_choice == 1):
+    print("You Win!")
+elif(user_choice == 2 && comp_choice == 2):
