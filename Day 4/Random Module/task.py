63bb565d31648832e6e20c7067ab38a63f2caecd
G2943 <vaibhavgole88@gmail.com>
2026-03-19 12:26:23 +0530

    random module

---
+++ Day 4/Random Module/task.py
@@ -0,0 +1,11 @@
+import random
+import my_module
+
+random_number = random.randint(1, 10)
+
+# print(random_number)
+# print(my_module.my_number)
+
+random_boolean = random.random("Heads", "Tails")
+
+print(random_boolean)
